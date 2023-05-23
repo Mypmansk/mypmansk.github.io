7eb16761dbb1981a5178b0119674f6cdc8b5c20d
Anton Shugaev <mypmansk@mail.ru>
2023-05-23 23:17:30 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+$(".scale__result").html($('input[type="range"]').val()),
+  $(document).on("input change", 'input[type="range"]', function () {
+    $(".scale__result").html($(this).val() + " %");
+  });
